4c8ee28fed60202f8bb84beb7a8f527a62508922
Jakub Opatowski <jakub.opatowski@gmail.com>
2019-06-24 15:25:26 +0200

    Setting up library prototype

---
+++ include/testClass.hpp
@@ -0,0 +1,13 @@
+#ifndef TESTCLASS_H
+#define TESTCLASS_H
+
+#include<string>
+
+class TestClass
+{
+public:
+    std::string hello();
+    std::string showMsg();
+};
+
+#endif /* TESTCLASS_H */
---
+++ src/testClass.cpp
@@ -0,0 +1,13 @@
+#include "testClass.hpp"
+
+std::string TestClass::showMsg() {
+    std::string b;
+    b = "takie tam...";
+    return b;
+}
+
+std::string TestClass::hello() {
+    std::string b;
+    b = "jakub.opatowski";
+    return b;
+}
